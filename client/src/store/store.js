dd029b4b542df1cbb2053d4134b3afb8619ca580
Nicholas Mitchell <nicholasmitchell@nicholass-MacBook-Pro.local>
2018-10-08 13:18:53 -0400

    Working

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import AuthenticationService from "../services/AuthenticationService";
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  //plugins: [createPersistedState()],
   state: {
     token: "",
     loginState: 1,
